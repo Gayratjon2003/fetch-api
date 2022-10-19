b4e4cff5bc520e01049e24953b8962198d6d7ff5
Gayratjon2003 <gayratjonabdijobborov@gmail.com>
2022-10-19 17:55:28 +0500

    task created and ended

---
+++ src/components/Home.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import './home.scss';
+function Home() {
+  const [usersData, setUsersData] = useState([]);
+  const [postsData, setPostsData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        setUsersData(res?.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(true);
+        setError(err);
+      });
+  }, []);
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => {
+        setPostsData(res?.data);
+        setData(postsData.filter((item) => item.userId == 1));
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setIsLoading(true);
+        setError(err);
+      });
+  }, [data]);
+  const onClickItem = (val) => {
+    const id = usersData
+      .filter((item) => item.name == val)
+      .map((item) => item.id)
+      .toString();
+    setData(postsData.filter((item) => item.userId == id));
+  };
+  return (
+    <>
+      {!isLoading && (
+        <div className="users-ui">
+          <div>
+            <h2>Data from JSON Placeholder</h2>
+            <select onChange={(e) => onClickItem(e.target.value)} id="select">
+              {usersData.map((data) => (
+                <option key={data.id}> {data.name} </option>
+              ))}
+            </select>
+          </div>
+          <div className="posts">
+            <ul>
+              {data?.map((post) => (
+                <li key={post.id}>{post.title}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Home;
